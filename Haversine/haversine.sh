4db7781fac6d7e1c2eaedf78c6735dc4f6b273b5
David Edson <eddo888@tpg.com.au>
2022-11-10 10:25:09 +1100

    ok

---
+++ Haversine/haversine.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+IFS=$'\n'
+
+username='eddo888'
+hostname='haversine.com'
+password=$(squirrel.py get "${username}@${hostname}")
+
+declare -a arguments
+arguments=($*)
+
+haversine.py ${arguments[0]} -u "${username}" -p "${password}" ${arguments[@]:1}
+
